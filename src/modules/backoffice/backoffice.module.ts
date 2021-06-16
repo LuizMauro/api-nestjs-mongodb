9f636df7f51910f63981a30064da8782db5ffb03
Luiz Mauro <luizmetec@hotmail.com>
2021-06-16 20:02:10 -0300

    add: address controller

--- src/modules/backoffice/backoffice.module.ts
+++ src/modules/backoffice/backoffice.module.ts
@@ -1,5 +1,7 @@
 import { Module } from '@nestjs/common';
 import { CustomerController } from './controllers/customer.controller';
+import { AddressController } from './controllers/address.controller';
+
 import { MongooseModule } from '@nestjs/mongoose';
 
 import { CustomerSchema } from './schemas/customer.schema';
@@ -8,6 +10,7 @@ import { UserSchema } from './schemas/user.schema';
 import { AccountService } from './services/account.service';
 import { CustomerService } from './services/customer.service';
 import { AddressService } from './services/address.service';
+import { PetService } from './services/pet.service';
 @Module({
   imports: [
     MongooseModule.forFeature([
@@ -21,7 +24,7 @@ import { AddressService } from './services/address.service';
       },
     ]),
   ],
-  controllers: [CustomerController],
-  providers: [AddressService, AccountService, CustomerService],
+  controllers: [CustomerController, AddressController],
+  providers: [AddressService, AccountService, CustomerService, PetService],
 })
 export class BackofficeModule {}
--- src/modules/backoffice/services/customer.service.ts
+++ src/modules/backoffice/services/customer.service.ts
@@ -2,8 +2,6 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { CustomerDocument, Customer } from '../models/customer.model';
-import { AddressDocument, Address } from '../models/address.model';
-import { Pet } from '../models/pet.model';
 import { QueryDTO } from '../dtos/query.dto';
 
 @Injectable()
@@ -15,56 +13,6 @@ export class CustomerService {
   async create(data: Customer): Promise<Customer> {
     const customer = new this.model(data);
     return await customer.save();
-  }
-
-  async addBillingAddress(document: string, data: Address): Promise<Customer> {
-    const options = { upsert: true };
-    return await this.model.findOneAndUpdate(
-      { document },
-      {
-        $set: {
-          billingAddress: data,
-        },
-      },
-      options,
-    );
-  }
-
-  async addShippingAddress(document: string, data: Address): Promise<Customer> {
-    const options = { upsert: true };
-    return await this.model.findOneAndUpdate(
-      { document },
-      {
-        $set: {
-          shippingAddress: data,
-        },
-      },
-      options,
-    );
-  }
-
-  async createPet(document: string, data: Pet): Promise<Customer> {
-    const options = { upsert: true, new: true };
-    return await this.model.findOneAndUpdate(
-      { document },
-      {
-        $push: {
-          pets: data,
-        },
-      },
-      options,
-    );
-  }
-
-  async updatePet(document: string, id: string, data: Pet): Promise<Customer> {
-    return await this.model.findOneAndUpdate(
-      { document, 'pets._id': id },
-      {
-        $set: {
-          'pets.$': data,
-        },
-      },
-    );
   }
 
   async findAll(): Promise<Customer[]> {
