1081b30759eadd94746c492f5aa0364beea7daee
Luiz Mauro <luizmetec@hotmail.com>
2021-06-26 23:04:28 -0300

    add: swagger'

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import * as compression from 'compression';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { CustomLoggerService } from './shared/services/custom-logger.service';
 
@@ -9,6 +10,18 @@ async function bootstrap() {
   });
 
   app.use(compression());
+
+  //Open API
+  const options = new DocumentBuilder()
+    .setTitle('PetShop API')
+    .setDescription('API do cuso 7180')
+    .setVersion('0.0.1')
+    .addTag('petshop')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(3333);
 }
 bootstrap();
